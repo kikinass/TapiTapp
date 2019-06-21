9761ea5f4d15a778fc0a3d5aeb0dfcfc5fa2672a
Wajih Tagourty <wajih.tagourty@gmail.com>
2019-06-21 18:56:59 +0200

    Setting up the app structure.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import Header from './partials/Header';
+import Footer from './partials/Footer';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
